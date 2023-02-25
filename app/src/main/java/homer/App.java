6b196abd5056e7b55b983bb6c48f7b1be3b2dbd8
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-02-25 11:38:27 +0100

    add usage of controller and present devices for testing

--- app/src/main/java/homer/App.java
+++ app/src/main/java/homer/App.java
@@ -2,6 +2,34 @@ package homer;
 
 public final class App {
     public static void main(String[] args) {
+
+        final ControllerImpl control = new ControllerImpl();
+
+        final var lock = new Lock();
+        control.connectDevice(lock);
+        System.out.println(lock);
+        lock.getState().lock();
+        System.out.println(lock);
+        System.out.println();
+
+        control.connectDevice(new Lock());
+        control.connectDevice(new Lock());
+        control.connectDevice(new Thermometer());
+        control.getDevices().forEach(d -> {
+            System.out.println(d + " | " + d.getState() + " | " + d.getState().getClass());
+        });
+        System.out.println();
+        control.getDevices().forEach(d -> {
+            if (d instanceof Lock) {
+                final var casted = (Lock) d;
+                casted.getState().lock();
+            }
+        });
+        System.out.println();
+        control.getDevices().forEach(d -> {
+            System.out.println(d + " | " + d.getState() + " | " + d.getState().getClass());
+        });
+        System.out.println();
     }
 
     public static String getGreeting() {
