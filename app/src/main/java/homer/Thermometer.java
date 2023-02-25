24b632ed8186248fe8f122b0b3fab857a684bc28
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-02-25 11:32:26 +0100

    [device] add basic temperature sensor implementation

---
+++ app/src/main/java/homer/Thermometer.java
@@ -0,0 +1,39 @@
+package homer;
+
+import homer.api.Device;
+import homer.api.DeviceInfo;
+
+public class Thermometer implements Device<Double> {
+
+    private final long ID = 0;
+    private final String TYPE = "Thermometer";
+    private final Double state = 15.0;
+
+    @Override
+    public DeviceInfo getInfo() {
+        return new DeviceInfo() {
+
+            @Override
+            public long getID() {
+                return ID;
+            }
+
+            @Override
+            public String getType() {
+                return TYPE;
+            }
+
+        };
+    }
+
+    @Override
+    public Double getState() {
+        return this.state;
+    }
+
+    @Override
+    public String toString() {
+        return "Device [" + getInfo().getID() + ", " + getInfo().getType() + ", " + state + "]";
+    }
+
+}
