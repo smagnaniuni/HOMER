24651a4c3e07c8b0a72da4485195e681d610a0c7
Simone Magnani <simone.magnani7@studio.unibo.it>
2023-03-01 19:12:41 +0100

    [clock] add javadoc

--- app/src/main/java/homer/api/Clock.java
+++ app/src/main/java/homer/api/Clock.java
@@ -5,10 +5,24 @@ import java.time.temporal.TemporalAmount;
 
 public interface Clock {
 
+    /**
+     * 
+     * @return The current local date and time of the clock.
+     */
     LocalDateTime getDateTime();
 
+    /**
+     * Sets the clock to a new local date and time.
+     * 
+     * @param dateTime The new local date and time to set.
+     */
     void setDateTime(LocalDateTime dateTime);
 
+    /**
+     * Updates the clock local date and time with a delta time amount.
+     * 
+     * @param dt How much time should be added to the current clock time.
+     */
     void updateTick(TemporalAmount dt);
 
 }
